da911bc13fe4cd97487d7bb4c11ebfb46d5f50b7
vujke <stefanvujovic93@gmail.com>
2016-05-16 11:36:47 +0200

    kontroler

--- src/kontroler/Kontroler.java
+++ src/kontroler/Kontroler.java
@@ -5,11 +5,33 @@
  */
 package kontroler;
 
+import domen.AbstractObjekat;
+import java.io.IOException;
+import java.util.List;
+import komunikacija.Komunikacija;
+import transfer.KlijentTransfer;
+import transfer.ServerTransfer;
+
 /**
  *
  * @author vujke
  */
 public class Kontroler {
     
+    Komunikacija kom = Komunikacija.vratiInstancu();
     
+   public List<AbstractObjekat> vratiSvaMesta() throws IOException, ClassNotFoundException{
+       KlijentTransfer kt = new KlijentTransfer();
+       kt.setOperacija(konstante.Konstante.VRATI_LISTU_MESTA);
+       kom.posaljiZahtev(kt);
+       ServerTransfer st = kom.procitajOdgovor();
+       
+       if(st.getUspesnostOperacije() == 1){
+           return (List<AbstractObjekat>) st.getPodaci();
+       } else {
+           return null;
+       }
+   }
+    
+   
 }
