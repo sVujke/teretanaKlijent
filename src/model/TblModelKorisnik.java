91a7cc343673f90554deb35972f86898238ad372
vujke <stefanvujovic93@gmail.com>
2016-05-18 23:38:56 +0200

    tblmodelkorisnik dodat

---
+++ src/model/TblModelKorisnik.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import domen.Korisnik;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author vujke
+ */
+public class TblModelKorisnik extends AbstractTableModel{
+    
+    List<Korisnik> korisnici;
+    String[] kolone = new String[]{"email","ime","username"};
+
+    @Override
+    public int getRowCount() {
+        return korisnici.size();
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        return kolone.length;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Korisnik kor = korisnici.get(rowIndex);
+        switch(columnIndex){
+            case 0: return kor.getEmail();
+            case 1: return kor.getIme();
+            case 2: return kor.getUsername();
+            default: return "n/a";
+        }
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
